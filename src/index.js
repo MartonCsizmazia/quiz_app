cf0e39c8b3e472f6f742235e3dc58aec14448e09
MartonCsizmazia <csmarton93@gmail.com>
2020-05-28 15:50:16 +0200

    5 random question displayed on screen

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React, {Component} from "react";
 import ReactDom from "react-dom";
 import "./assets/style.css";
+import quizService from "./quizService";
 
 class  Quiz extends Component{
+    state = {
+        questionBank: []
+    };
+    getQuestions = () =>{
+        quizService().then(question => {
+            this.setState({
+              questionBank: question
+            });
+        });
+    };
+
+    componentDidMount() {
+        this.getQuestions();
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="title">Quiz</div>
-
+                {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, correct, questionId}) => (<h4>{question}</h4>))}
             </div>
         );
     }
-
 }
-
 
 ReactDom.render(<Quiz/>, document.getElementById("root"));
